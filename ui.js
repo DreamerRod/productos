ff8d16489d4671655038383ba6efc9e84b049a0e
Rodrigo Bejarano <rodribej.2@gmail.com>
2020-05-13 05:04:23 -0400

    añadir cambio a los formularios y el ui

--- ui.js
+++ ui.js
@@ -6,6 +6,12 @@ class Ui {
         this.uiQuantity = document.getElementById("quantity");
         this.uiForm = document.getElementById("form-data");
         this.container = document.getElementById("container-table");
+        //this.manager =  new ProductsManagement();
+        this.uiName1 = document.getElementById("namep");
+        this.uiName2 = document.getElementById("namepa");
+        this.uiForm2 = document.getElementById("form-data2");
+        this.uiName3 = document.getElementById("namere");
+        this.uiForm3 = document.getElementById("form-data3");
         this.manager =  new ProductsManagement();
         let p1 = new Products("Pollo", "Pollo Sofia", 20);
         let p2 = new Products("Galletas", "Galleras Mabel", 50);
@@ -22,6 +28,19 @@ class Ui {
                 this.uiName.value,
                 this.uiDescription.value,
                 this.uiQuantity.value);
+            this.clearForm();
+        });
+        this.uiForm2.addEventListener("submit", (e) => {
+            e.preventDefault();
+            this.updateProducts(
+                this.uiName1.value,
+                this.uiName2.value);
+            this.clearForm();
+        });
+        this.uiForm3.addEventListener("submit", (e) => {
+            e.preventDefault();
+            this.removeProducts(
+                this.uiName3.value);
             this.clearForm();
         });
     }
